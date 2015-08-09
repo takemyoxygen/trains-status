a45684855b534269009d914d83defd774ff12285
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2015-08-09 14:12:30 +0200

    added a method to get direction status

---
+++ src/playground/fs/Http.fsx
@@ -0,0 +1,8 @@
+#load "Credentials.fsx"
+#r "../packages/Fsharp.Data/lib/net40/FSharp.Data.dll"
+
+open FSharp.Data
+
+let private auth = HttpRequestHeaders.BasicAuth Credentials.username Credentials.password
+
+let get url query = Http.RequestString(url, query = query, headers = [auth])
