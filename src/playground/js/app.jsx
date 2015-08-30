13b2f721777bc82a1f2a6f7d38cb009f70bfccf6
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2015-08-30 22:55:01 +0200

    added demo version of the main page

--- src/playground/js/app.jsx
+++ src/playground/js/app.jsx
@@ -3,13 +3,13 @@ var Origin = React.createClass({
     return {station: "..."};
   },
   componentDidMount: function(){
-    Stations.load()
+    Stations.loadNearby()
       .done(function(stations){
         if (this.isMounted() && stations.length && stations.length > 0){
-          var station = stations[0].name;
-          this.setState({station: station});
+          var origin = stations[0];
+          this.setState({station: origin.name});
           if (typeof this.props.onStationSelected === "function"){
-            this.props.onStationSelected(station);
+            this.props.onStationSelected(origin);
           }
         }
       }.bind(this));
@@ -19,9 +19,105 @@ var Origin = React.createClass({
   }
 });
 
-var DirectionsStatus = React.createClass({
+var Direction = React.createClass({
+  getInitialState: () => ({status: "loading"}),
+  labelType: (status) => {
+    switch (status){
+      case "ok": return "success";
+      case "delayed": return "warning";
+      case "notfound": return "danger";
+      default: return "info";
+    }
+  },
+  iconType: (status) => {
+    switch (status){
+      case "ok": return "ok-sign";
+      case "delayed": return "alert";
+      case "notfound": return "remove-sign";
+      default: return "question-sign";
+    }
+  },
+  componentDidMount: function(){
+    this.originSubscription = this.props.origin
+      .filter(s => s != null)
+      .do(function(s){
+        this.setState({disabled: s.name == this.props.destination.name});
+      }.bind(this))
+      .filter(function(s){
+        return s.name != this.props.destination.name;
+      }.bind(this))
+      .subscribe(function(origin){
+      this.checkStatus(origin);
+    }.bind(this));
+  },
+  checkStatus: function(origin){
+    console.log("Checkig status...");
+    $.getJSON("/api/status/" + origin.name + "/" + this.props.destination.name)
+      .done(function(response){
+        if (this.isMounted()){
+            this.setState({status: response.status.toLowerCase()});
+        }
+      }.bind(this));
+  },
   render: function(){
-    return <Origin onStationSelected={function(s){console.log(s);}}/>;
+    return (
+      <li className={"list-group-item" + (this.state.disabled ? " disabled": "")}>
+        <div className="row">
+          <div className="col-md-10">
+            {this.props.destination.name}
+          </div>
+          <div className={"col-md-1" + (this.state.disabled ? " hidden" : "")}>
+            <span className={"label label-" + this.labelType(this.state.status)}>
+              <span className={"glyphicon glyphicon-" + this.iconType(this.state.status)} aria-hidden="true"></span>
+            </span>
+          </div>
+        </div>
+      </li>
+    );
+  }
+})
+
+var FavouritesStatus = React.createClass({
+  componentDidMount: function(){
+    Stations
+      .loadFavourites()
+      .done(function(stations){
+        this.setState({stations: stations});
+      }.bind(this));
+  },
+  getInitialState: function(){
+    return {stations: []};
+  },
+  getDefaultProps: function(){
+    return {};
+  },
+  render: function(){
+      return (
+        <div className="row">
+          <div className="col-md-5">
+            <ul className="list-group">
+              {this.state.stations.map(function(s){
+                return <Direction origin={this.props.origin} destination={s} />;
+              }.bind(this))}
+            </ul>
+          </div>
+        </div>);
+  }
+});
+
+var DirectionsStatus = React.createClass({
+  onOriginChanged: function(station){
+    this.props.origin.onNext(station);
+  },
+  getDefaultProps: function(){
+    return {origin: new Rx.BehaviorSubject(null)};
+  },
+  render: function(){
+    return (
+      <div>
+        <Origin onStationSelected={this.onOriginChanged}/>
+        <FavouritesStatus origin={this.props.origin}/>
+      </div>);
   }
 });
 
--- src/playground/js/stations.js
+++ src/playground/js/stations.js
@@ -1,12 +1,12 @@
 (function(Stations){
   "use strict";
 
-  var endpoint = "/api/stations/nearby?count=5"
 
-  Stations.load = function(){
+  Stations.loadNearby = function(){
     return $.Deferred(function(deferred){
       if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(location){
+            var endpoint = "/api/stations/nearby?count=5"
             var url = endpoint + "&lat=" + location.coords.latitude + "&lon=" + location.coords.longitude;
             $.getJSON(url).then(deferred.resolve, deferred.reject)
           });
@@ -14,5 +14,9 @@
           deferred.reject("Geolocation is not supported by this browser.");
       }
     });
+  }
+
+  Stations.loadFavourites = function(){
+    return $.getJSON("/api/stations/favourite");
   }
 })(window.Stations = window.Stations || {});
