731c1e3a0d36122685655ce7791fceb1bdcc0e8a
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2015-10-04 16:02:11 +0200

    Render login button using React

--- src/playground/js/src/view/main.jsx
+++ src/playground/js/src/view/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import DirectionsStatus from "view/favourite-directions"
+import User from "view/user"
 
 export default class Main extends React.Component{
-    render = () => <DirectionsStatus />
+    render = () => (
+        <div>
+            <User />
+            <DirectionsStatus />
+        </div>)
 };
---
+++ src/playground/js/src/view/user.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import $ from "jquery";
+
+export default class User extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {loggedIn: false};
+    }
+
+    componentWillMount(){
+        $('head').append(`<meta name="google-signin-client_id" content="1070118148604-54uf2ocdsog6qsbigomu22v42aujahht.apps.googleusercontent.com">`);
+        $('head').append(`<script src="https://apis.google.com/js/platform.js" async defer></script>`);
+    }
+
+    componentDidMount(){
+        this.tryRenderSignIn();
+    }
+
+    tryRenderSignIn(){
+        setTimeout(() => {
+            if (typeof gapi === "undefined"){
+                console.log("No gapi yet.");
+                this.tryRenderSignIn();
+            } else {
+                this.renderSignIn();
+            }
+        }, 100)
+    }
+
+    renderSignIn(){
+        gapi.signin2.render('sign-in', {
+            'scope': 'https://www.googleapis.com/auth/plus.login',
+            'width': 100,
+            'height': 30,
+            'longtitle': false,
+            'theme': 'dark',
+            'onsuccess': this.signIn,
+        });
+    }
+
+    signIn = googleUser => {
+        var profile = googleUser.getBasicProfile();
+        var token = googleUser.getAuthResponse().id_token;
+        var name = profile.getName();
+        $.get("/api/user/info?token=" + token)
+            .then(() => this.setState({loggedIn: true, username: name}));
+    }
+
+    signOut = () => {
+        gapi.auth2
+            .getAuthInstance()
+            .signOut()
+            .then(() => {
+                console.log('User signed out.');
+                this.setState({loggedIn: false});
+            });
+    }
+
+    render(){
+        return (
+            <div className="user">
+                <div id="sign-in" className={this.state.loggedIn ? "hidden": ""}></div>
+                <div className={`username ${this.state.loggedIn ? "": "hidden"}`}>
+                    <span>Hello {this.state.username}</span>
+                    <a href="#" onClick={this.signOut}>Sign out</a>
+                </div>
+            </div>
+        );
+    }
+}
