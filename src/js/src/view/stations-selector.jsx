04c4f8c4794d343368645d36c67e022bfc31ca88
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2015-11-24 20:43:39 +0100

    Use react-select and clean-up code

--- src/js/src/view/stations-selector.jsx
+++ src/js/src/view/stations-selector.jsx
@@ -5,9 +5,9 @@ import Stations from "stations";
 import Select from "react-select";
 
 export default class StationsSelector extends React.Component{
-    constructor(){
+    constructor(props){
         super();
-        this.state = {stations: [], valid: true, text: ""};
+        this.state = {stations: [], valid: true, currentStation: props.currentStation};
     }
 
     static propTypes = {
@@ -24,43 +24,29 @@ export default class StationsSelector extends React.Component{
             .done(stations => this.setState({stations: stations}));
     }
 
-    componentDidUpdate(){
-        if (this.props.autofocus){
-            $(ReactDOM.findDOMNode(this))
-                .find("input[role=combobox]")
-                .focus();
+    onChange = station => {
+        console.log("Current station changed to:" + station.name);
+        let valid = this.props.canSelectStation(station);
+        if (valid){
+            this.props.onStationSelected(station);
         }
+        this.setState({valid: valid, currentStation: station});
     }
-
-    onSelect = (_, item) => {
-        if (this.props.canSelectStation(item)){
-            this.props.onStationSelected(item);
-        } else {
-            this.setState({valid: false});
-        }
-    }
-
-    onOk = () => {
-        const current =
-            this.state.text &&
-            this.state.valid &&
-            this.state.stations.find(station => station.name.toLowerCase() == this.state.text.toLowerCase())
-
-         if (current) this.onSelect(null, current)
-         else this.setState({valid: false});
-    }
-
-    onChange = (_, text) => this.setState({valid: true, text: text});
 
     render(){
-        var items = [
-            {key: "k1", value: "v1"},
-            {key: "k2", value: "v2"}
-        ];
         return (
             <div className={`stations-selector ${this.state.valid ? "valid" : "invalid"}`}>
-                <Select name="some-select" options={items} value="please select something..." autofocus clearable simpleValue />
-                <a className="btn btn-primary" onClick={this.onOk}>Ok</a>
+                <div className="stations-dropdown">
+                    <Select
+                        name="some-select"
+                        options={this.state.stations}
+                        value={this.state.currentStation}
+                        autofocus
+                        searchable={true}
+                        clearable={false}
+                        labelKey="name"
+                        onChange={this.onChange} />
+                </div>
                 <a className="btn btn-default" onClick={this.props.onCancel && this.props.onCancel}>Cancel</a>
             </div>);
     }
