1cb09aa232e5376c289e5146bb21c3b649ac5ef8
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-09-26 22:49:08 +0200

    Consolidate functions deployment in existing build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,15 @@
 @echo off
 cls
 
-@echo Restoring Paket dependencies
-.paket\paket.bootstrapper.exe
-.paket\paket.exe restore group AzureFunctions
+if "%FUNCTIONS_EXTENSION_VERSION%"=="" (
+    @echo Restoring Paket dependencies
+    .paket\paket.bootstrapper.exe
+    .paket\paket.exe restore group AzureFunctions
 
-"packages\azurefunctions\FAKE\tools\Fake.exe" build.fsx %*
-exit /b %errorlevel%
+    "packages\azurefunctions\FAKE\tools\Fake.exe" build.fsx %*
+    exit /b %errorlevel%
+) else (
+    echo Deploying functions.
+    xcopy src\functions %DEPLOYMENT_TARGET% /s /f
+)
+
--- src/deploy-functions.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo Deploying functions...
-xcopy src\functions %DEPLOYMENT_TARGET% /s /f
