99e039b36f473065e18c435a7b46310fc5a663b5
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-09-27 20:47:56 +0200

    Remove old functions when deploying new ones

--- build.bat
+++ build.bat
@@ -9,7 +9,15 @@ if "%FUNCTIONS_EXTENSION_VERSION%"=="" (
     "packages\azurefunctions\FAKE\tools\Fake.exe" build.fsx %*
     exit /b %errorlevel%
 ) else (
-    echo Deploying functions.
+    echo Deploying functions
+
+    echo Deleting old stuff...
+    for /f %%i in ('dir /b /a:d %DEPLOYMENT_TARGET%') do (
+        echo Deleting %DEPLOYMENT_TARGET%\%%i...
+        rd %DEPLOYMENT_TARGET%\%%i /s/q
+    )
+
+    echo Copying new stuff...
     xcopy src\functions %DEPLOYMENT_TARGET% /s /f /Y
 )
 
