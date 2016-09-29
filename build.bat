40d2f4fa636000f5173f0e39f8d1d1388340873f
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-09-29 08:09:40 +0200

    Use different dedicated environment variable to determine which part of application should be deployed

--- build.bat
+++ build.bat
@@ -1,23 +1,23 @@
 @echo off
 cls
 
-if "%FUNCTIONS_EXTENSION_VERSION%"=="" (
+if "%ENVIRONMENT_TYPE%"=="azure-functions" (
+    @echo Deploying functions...
+
+    @echo Deleting old stuff...
+    for /f %%i in ('dir /b /a:d %DEPLOYMENT_TARGET%') do (
+        @echo Deleting %DEPLOYMENT_TARGET%\%%i...
+        rd %DEPLOYMENT_TARGET%\%%i /s/q
+    )
+
+    @echo Copying new stuff...
+    xcopy src\functions %DEPLOYMENT_TARGET% /s /f /Y
+) else (
     @echo Restoring Paket dependencies
     .paket\paket.bootstrapper.exe
     .paket\paket.exe restore group AzureFunctions
 
     "packages\azurefunctions\FAKE\tools\Fake.exe" build.fsx %*
     exit /b %errorlevel%
-) else (
-    echo Deploying functions
-
-    echo Deleting old stuff...
-    for /f %%i in ('dir /b /a:d %DEPLOYMENT_TARGET%') do (
-        echo Deleting %DEPLOYMENT_TARGET%\%%i...
-        rd %DEPLOYMENT_TARGET%\%%i /s/q
-    )
-
-    echo Copying new stuff...
-    xcopy src\functions %DEPLOYMENT_TARGET% /s /f /Y
 )
 
